46e1eb21c3d4d5f07ae458400a20799e13ff3250
Jagonmoy Dey <jagonmoy25@student.sust.edu>
2021-06-15 18:16:34 +0600

    Add files via upload

---
+++ Server/src/Data/DataInitFile.java
@@ -0,0 +1,32 @@
+
+package Data;
+
+
+public class DataInitFile {
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public DataInitFile(String fileName, long fileSize) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+    }
+
+    public DataInitFile() {
+    }
+
+    private String fileName;
+    private long fileSize;
+}
